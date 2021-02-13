0b37be8d995a2dcf41aee4292838cde4503ce6a9
Augusto Luna <caugustolunas@gmail.com>
2021-02-13 13:22:05 -0300

    Importei dados JSON, criei GameCard e criei o globalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Header from "./components/Header/Header";
+import GameCard from "./components/GameCard/GameCard";
+
+import products from "./data/products.json";
 
 function App() {
   return (
     <>
       <Header />
+      {products.map((product) => (
+        <GameCard
+          key={product.id}
+          image={product.image}
+          name={product.name}
+          price={product.price}
+          score={product.score}
+        />
+      ))}
     </>
   );
 }
